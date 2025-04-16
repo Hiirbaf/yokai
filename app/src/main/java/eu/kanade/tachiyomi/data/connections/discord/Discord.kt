e4ca011a5e2e21b4da7968f105c9ef6930a844b4
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-16 17:46:55 -0300

    Update Discord.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/Discord.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/Discord.kt
@@ -3,10 +3,11 @@ package eu.kanade.tachiyomi.data.connections.discord
 import android.graphics.Color
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.connections.ConnectionsService
+import tachiyomi.i18n.MR
 
 class Discord(id: Long) : ConnectionsService(id) {
 
-    override fun nameRes() = R.strings.connections_discord
+    override fun nameRes() = MR.strings.connections_discord
 
     override fun getLogo() = R.drawable.ic_discord_24dp
 
