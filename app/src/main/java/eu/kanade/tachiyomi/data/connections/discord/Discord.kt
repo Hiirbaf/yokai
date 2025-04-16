1af18b10dd067f3d2989d45747929f4c2d4fac16
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-16 16:08:31 -0300

    [skip ci] Update Discord.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/Discord.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/Discord.kt
@@ -3,7 +3,7 @@ package eu.kanade.tachiyomi.data.connections.discord
 import android.graphics.Color
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.connections.ConnectionsService
-import tachiyomi.i18n.commonMain.moko-resources.base
+import yokai.i18n.commonMain.base
 
 class Discord(id: Long) : ConnectionsService(id) {
 
