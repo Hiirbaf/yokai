a1e2ee5099407f3a7fdfee645d7422736cc99061
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-24 02:07:25 -0300

    Update CategoryExtensions.kt

--- app/src/main/java/yokai/presentation/extension/repo/CategoryExtensions.kt
+++ app/src/main/java/yokai/presentation/extension/repo/CategoryExtensions.kt
@@ -6,6 +6,7 @@ import dev.icerock.moko.resources.compose.stringResource
 import yokai.domain.category.model.Category
 import yokai.i18n.MR
 import java.lang.String.format
+import kotlin.text.format
 
 val Category.visualName: String
     @Composable
