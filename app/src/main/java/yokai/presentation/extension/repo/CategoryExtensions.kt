a7332ca969d251566d7237f85db874be6e6928c4
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-24 02:44:45 -0300

    Update CategoryExtensions.kt

--- app/src/main/java/yokai/presentation/extension/repo/CategoryExtensions.kt
+++ app/src/main/java/yokai/presentation/extension/repo/CategoryExtensions.kt
@@ -1,30 +1,15 @@
 package yokai.presentation.extension.repo
 
 import android.content.Context
-import dev.icerock.moko.resources.StringResource
-import dev.icerock.moko.resources.desc.StringDesc
-import dev.icerock.moko.resources.compose.stringResource
-import dev.icerock.moko.resources.getString
 import androidx.compose.runtime.Composable
+import dev.icerock.moko.resources.compose.stringResource
+import yokai.domain.category.model.Category
+import yokai.i18n.MR
 
-fun Context.localizedString(resource: StringResource, vararg args: Any): String {
-    return StringDesc.ResourceFormatted(resource, *args).toString(this)
-}
-
-// Versión para Context
-fun Context.localizedString(resource: StringResource, vararg args: Any): String {
-    return resource.getString(this, *args)
-}
-
-// Versión Composable
-@Composable
-fun localizedString(resource: StringResource, vararg args: Any): String {
-    return stringResource(resource, *args)
-}
 val Category.visualName: String
     @Composable
     get() = when {
-        isSystemCategory -> localizedString(MR.strings.label_default, name)
+        isSystemCategory -> stringResource(MR.strings.label_default)
         else -> name
     }
 
