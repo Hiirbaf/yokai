5c77948bef792ff9f7b3d474ce7495ac68af3681
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-24 14:26:00 -0300

    Update SettingsConnectionsScreen.kt

--- app/src/main/java/yokai/presentation/settings/screen/SettingsConnectionsScreen.kt
+++ app/src/main/java/yokai/presentation/settings/screen/SettingsConnectionsScreen.kt
@@ -48,6 +48,7 @@ import kotlinx.collections.immutable.persistentListOf
 import eu.kanade.tachiyomi.util.system.launchIO
 import eu.kanade.tachiyomi.util.system.withUIContext
 import yokai.i18n.MR
+import yokai.presentation.settings.ComposableSettings
 import tachiyomi.presentation.core.i18n.stringResource
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
