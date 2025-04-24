db6df03cc221ff5cc83106b74648c945aa814b62
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-24 03:15:47 -0300

    Update CategoryExtensions.kt

--- app/src/main/java/yokai/presentation/extension/repo/CategoryExtensions.kt
+++ app/src/main/java/yokai/presentation/extension/repo/CategoryExtensions.kt
@@ -3,14 +3,14 @@ package yokai.presentation.extension.repo
 import android.content.Context
 import androidx.compose.runtime.Composable
 import dev.icerock.moko.resources.compose.stringResource
-import yokai.domain.category.model.Category
-import yokai.i18n.MR
 import dev.icerock.moko.resources.StringResource
 import dev.icerock.moko.resources.desc.StringDesc
-import dev.icerock.moko.resources.desc.desc
+import dev.icerock.moko.resources.desc.toLocalizedString
+import yokai.domain.category.model.Category
+import yokai.i18n.MR
 
 fun Context.localizedString(resource: StringResource, vararg args: Any): String {
-    return StringDesc.Resource(resource, args.toList()).toString(this)
+    return StringDesc.Resource(resource, args.toList()).toLocalizedString(this)
 }
 
 val Category.visualName: String
