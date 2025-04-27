a83b03d858e1c171cb2210cb4859006fb3ee45f1
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-26 21:00:25 -0300

    Update AboutScreen.kt

--- app/src/main/java/yokai/presentation/settings/screen/about/AboutScreen.kt
+++ app/src/main/java/yokai/presentation/settings/screen/about/AboutScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
