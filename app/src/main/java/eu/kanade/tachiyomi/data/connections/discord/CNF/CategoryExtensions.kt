90937fe6663553d696f6b5d34d7987f5be3498b5
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-28 02:28:55 -0300

    Update CategoryExtensions.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/CNF/CategoryExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/CNF/CategoryExtensions.kt
@@ -10,12 +10,12 @@ import dev.icerock.moko.resources.compose.stringResource
 val Category.visualName: String
     @Composable
     get() = when {
-        isSystemCategory -> stringResource(MR.strings.label_default)
+        isDefaultSystemCategory -> stringResource(MR.strings.label_default)
         else -> name
     }
 
 fun Category.visualName(context: Context): String =
     when {
-        isSystemCategory -> context.stringResource(MR.strings.label_default)
+        isDefaultSystemCategory -> context.stringResource(MR.strings.label_default)
         else -> name
     }
