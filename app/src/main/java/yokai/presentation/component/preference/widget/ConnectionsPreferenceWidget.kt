c1713e78bce7a7b81600a06878c4d54e6f5bc8d7
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-28 06:13:38 -0300

    Update ConnectionsPreferenceWidget.kt

--- app/src/main/java/yokai/presentation/component/preference/widget/ConnectionsPreferenceWidget.kt
+++ app/src/main/java/yokai/presentation/component/preference/widget/ConnectionsPreferenceWidget.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import yokai.presentation.component.ConnectionsLogoIcon
-import yokai.presentation.more.settings.LocalPreferenceHighlighted
+import yokai.presentation.component.preference.LocalPreferenceHighlighted
 import eu.kanade.tachiyomi.data.connections.ConnectionsService
 import yokai.i18n.MR
 import dev.icerock.moko.resources.compose.stringResource
