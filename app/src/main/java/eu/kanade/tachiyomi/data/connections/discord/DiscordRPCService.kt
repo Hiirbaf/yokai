41d149a802ff844605dafc642123db5f6f6677e5
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-27 21:01:55 -0300

    Update DiscordRPCService.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/DiscordRPCService.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/connections/discord/DiscordRPCService.kt
@@ -147,7 +147,7 @@ class DiscordRPCService : Service() {
             if (rpc == null || readerData.thumbnailUrl == null || readerData.mangaId == null) return
 
             val categoryIds = Injekt.get<GetCategories>()
-                .await(readerData.mangaId)
+                .awaitByMangaId(readerData.mangaId)
                 .map { it.id.toString() }
                 .run { ifEmpty { plus(UNCATEGORIZED_ID.toString()) } }
 
