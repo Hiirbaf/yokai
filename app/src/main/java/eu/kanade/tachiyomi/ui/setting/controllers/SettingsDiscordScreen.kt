41185e74faf5850628a8085d12278e250f425143
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-04-28 17:44:13 -0300

    Update SettingsDiscordScreen.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
@@ -26,7 +26,7 @@ import kotlinx.coroutines.runBlocking
 import tachiyomi.domain.category.interactor.GetCategories
 import yokai.i18n.MR
 import tachiyomi.presentation.core.i18n.stringResource
-import tachiyomi.presentation.core.util.collectAsState
+import eu.kanade.tachiyomi.core.storage.preference.collectAsState
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
