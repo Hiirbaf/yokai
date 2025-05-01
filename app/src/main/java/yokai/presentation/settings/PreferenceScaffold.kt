12c960f00b442c98a91e47301dd02089dce4554e
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-01 01:22:38 -0300

    Create PreferenceScaffold.kt

---
+++ app/src/main/java/yokai/presentation/settings/PreferenceScaffold.kt
@@ -0,0 +1,33 @@
+package yokai.presentation.settings
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.runtime.Composable
+import dev.icerock.moko.resources.StringResource
+import yokai.presentation.component.AppBar
+import yokai.presentation.Scaffold
+import dev.icerock.moko.resources.compose.stringResource
+
+@Composable
+fun PreferenceScaffold(
+    titleRes: StringResource,
+    actions: @Composable RowScope.() -> Unit = {},
+    onBackPressed: (() -> Unit)? = null,
+    itemsProvider: @Composable () -> List<Preference>,
+) {
+    Scaffold(
+        topBar = {
+            AppBar(
+                title = stringResource(titleRes),
+                navigateUp = onBackPressed,
+                actions = actions,
+                scrollBehavior = it,
+            )
+        },
+        content = { contentPadding ->
+            PreferenceScreen(
+                items = itemsProvider(),
+                contentPadding = contentPadding,
+            )
+        },
+    )
+}
