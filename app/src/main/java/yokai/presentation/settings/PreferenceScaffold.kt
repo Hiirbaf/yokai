b635ecfb30cf5703cc226bc085bccde1abfd4186
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-01 13:14:32 -0300

    Update PreferenceScaffold.kt

--- app/src/main/java/yokai/presentation/settings/PreferenceScaffold.kt
+++ app/src/main/java/yokai/presentation/settings/PreferenceScaffold.kt
@@ -8,6 +8,25 @@ import androidx.compose.material3.Scaffold
 import dev.icerock.moko.resources.compose.stringResource
 
 @Composable
+fun PreferenceScreen(
+    items: List<Preference>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    val state = rememberLazyListState()
+    val highlightKey = SearchableSettings.highlightKey
+    if (highlightKey != null) {
+        LaunchedEffect(Unit) {
+            val i = items.findHighlightedIndex(highlightKey)
+            if (i >= 0) {
+                delay(0.5.seconds)
+                state.animateScrollToItem(i)
+            }
+            SearchableSettings.highlightKey = null
+        }
+    }
+
+@Composable
 fun PreferenceScaffold(
     titleRes: StringResource,
     actions: @Composable RowScope.() -> Unit = {},
