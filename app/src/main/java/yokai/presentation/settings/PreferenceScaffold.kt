a58084b946d972aed30dd6240353341008d1d7ce
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-01 12:38:19 -0300

    Update PreferenceScaffold.kt

--- app/src/main/java/yokai/presentation/settings/PreferenceScaffold.kt
+++ app/src/main/java/yokai/presentation/settings/PreferenceScaffold.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
 import dev.icerock.moko.resources.StringResource
 import yokai.presentation.component.AppBar
-import yokai.presentation.settings.SettingsScaffold
+import androidx.compose.material3.Scaffold
 import dev.icerock.moko.resources.compose.stringResource
 
 @Composable
@@ -14,13 +14,13 @@ fun PreferenceScaffold(
     onBackPressed: (() -> Unit)? = null,
     itemsProvider: @Composable () -> List<Preference>,
 ) {
-    YokaiScaffold(
+    Scaffold(
         topBar = {
             AppBar(
                 title = stringResource(titleRes),
                 navigateUp = onBackPressed,
                 actions = actions,
-                scrollBehavior = it,
+                //scrollBehavior = it,
             )
         },
         content = { contentPadding ->
