2454de5e782d6988d5b773928c7d7ce87264c68c
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-03 13:10:23 -0300

    Update SettingsDiscordScreen.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
@@ -1,193 +1,169 @@
 package eu.kanade.tachiyomi.ui.setting.controllers
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.HelpOutline
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.util.fastMap
 import yokai.domain.connections.service.ConnectionsPreferences
+import eu.kanade.presentation.category.visualName
 import yokai.presentation.component.preference.Preference
 import yokai.presentation.component.preference.widget.TriStateListDialog
-import eu.kanade.tachiyomi.core.storage.preference.collectAsState
 import eu.kanade.tachiyomi.data.connections.ConnectionsManager
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.coroutines.runBlocking
+import tachiyomi.domain.category.interactor.GetCategories
 import yokai.i18n.MR
 import dev.icerock.moko.resources.compose.stringResource
+import tachiyomi.presentation.core.util.collectAsState
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
-import yokai.domain.category.interactor.GetCategories
-import eu.kanade.tachiyomi.util.compose.LocalBackPress
-import androidx.glance.LocalContext
 
-@Composable
-fun SettingsDiscordScreen() {
-    val uriHandler = LocalUriHandler.current
-    val connectionsPreferences = remember { Injekt.get<ConnectionsPreferences>() }
-    val connectionsManager = remember { Injekt.get<ConnectionsManager>() }
-    val enableDRPCPref = connectionsPreferences.enableDiscordRPC()
-    val useChapterTitlesPref = connectionsPreferences.useChapterTitles()
-    val discordRPCStatus = connectionsPreferences.discordRPCStatus()
+object SettingsDiscordScreen : BaseComposeController {
 
-    val enableDRPC by enableDRPCPref.collectAsState()
-    val useChapterTitles by useChapterTitlesPref.collectAsState()
+@ReadOnlyComposable  
+@Composable  
+override fun getTitleRes() = MR.strings.pref_category_connections  
 
-    var dialog by remember { mutableStateOf<Any?>(null) }
-    dialog?.run {
-        when (this) {
-            is LogoutConnectionsDialog -> {
-                ConnectionsLogoutDialog(
-                    service = service,
-                    onDismissRequest = {
-                        dialog = null
-                        enableDRPCPref.set(false)
-                    },
-                )
-            }
-        }
-    }
+@Composable  
+override fun RowScope.AppBarAction() {  
+    val uriHandler = LocalUriHandler.current  
+    IconButton(onClick = { uriHandler.openUri("https://tachiyomi.org/help/guides/tracking/") }) {  
+        Icon(  
+            imageVector = Icons.AutoMirrored.Outlined.HelpOutline,  
+            contentDescription = stringResource(MR.strings.tracking_guide),  
+        )  
+    }  
+}  
 
-    Column {
-        PreferenceGroup(
-            title = stringResource(MR.strings.connections_discord),
-            preferenceItems = persistentListOf(
-                Preference.PreferenceItem.SwitchPreference(
-                    preference = enableDRPCPref,
-                    title = stringResource(MR.strings.pref_enable_discord_rpc),
-                ),
-                Preference.PreferenceItem.SwitchPreference(
-                    preference = useChapterTitlesPref,
-                    enabled = enableDRPC,
-                    title = stringResource(MR.strings.show_chapters_titles_title),
-                    subtitle = stringResource(MR.strings.show_chapters_titles_subtitle),
-                ),
-                Preference.PreferenceItem.ListPreference(
-                    preference = discordRPCStatus,
-                    title = stringResource(MR.strings.pref_discord_status),
-                    entries = persistentMapOf(
-                        -1 to stringResource(MR.strings.pref_discord_dnd),
-                        0 to stringResource(MR.strings.pref_discord_idle),
-                        1 to stringResource(MR.strings.pref_discord_online),
-                    ),
-                    enabled = enableDRPC,
-                ),
-            ),
-        )
-        getRPCIncognitoGroup(
-            connectionsPreferences = connectionsPreferences,
-            enabled = enableDRPC,
-        )
-        PreferenceItem.TextPreference(
-            title = stringResource(MR.strings.logout),
-            onClick = { dialog = LogoutConnectionsDialog(connectionsManager.discord) },
-        )
-    }
+@Composable  
+override fun getPreferences(): List<Preference> {  
+    val connectionsPreferences = remember { Injekt.get<ConnectionsPreferences>() }  
+    val connectionsManager = remember { Injekt.get<ConnectionsManager>() }  
+    val enableDRPCPref = connectionsPreferences.enableDiscordRPC()  
+    val useChapterTitlesPref = connectionsPreferences.useChapterTitles()  
+    val discordRPCStatus = connectionsPreferences.discordRPCStatus()  
+
+    val enableDRPC by enableDRPCPref.collectAsState()  
+    val useChapterTitles by useChapterTitlesPref.collectAsState()  
+
+    var dialog by remember { mutableStateOf<Any?>(null) }  
+    dialog?.run {  
+        when (this) {  
+            is LogoutConnectionsDialog -> {  
+                ConnectionsLogoutDialog(  
+                    service = service,  
+                    onDismissRequest = {  
+                        dialog = null  
+                        enableDRPCPref.set(false)  
+                    },  
+                )  
+            }  
+        }  
+    }  
+
+    return listOf(  
+        Preference.PreferenceGroup(  
+            title = stringResource(MR.strings.connections_discord),  
+            preferenceItems = persistentListOf(  
+                Preference.PreferenceItem.SwitchPreference(  
+                    preference = enableDRPCPref,  
+                    title = stringResource(MR.strings.pref_enable_discord_rpc),  
+                ),  
+                Preference.PreferenceItem.SwitchPreference(  
+                    preference = useChapterTitlesPref,  
+                    enabled = enableDRPC,  
+                    title = stringResource(MR.strings.show_chapters_titles_title),  
+                    subtitle = stringResource(MR.strings.show_chapters_titles_subtitle),  
+                ),  
+                Preference.PreferenceItem.ListPreference(  
+                    preference = discordRPCStatus,  
+                    title = stringResource(MR.strings.pref_discord_status),  
+                    entries = persistentMapOf(  
+                        -1 to stringResource(MR.strings.pref_discord_dnd),  
+                        0 to stringResource(MR.strings.pref_discord_idle),  
+                        1 to stringResource(MR.strings.pref_discord_online),  
+                    ),  
+                    enabled = enableDRPC,  
+                ),  
+            ),  
+        ),  
+        getRPCIncognitoGroup(  
+            connectionsPreferences = connectionsPreferences,  
+            enabled = enableDRPC,  
+        ),  
+        Preference.PreferenceItem.TextPreference(  
+            title = stringResource(MR.strings.logout),  
+            onClick = { dialog = LogoutConnectionsDialog(connectionsManager.discord) },  
+        ),  
+    )  
+}  
+
+@Composable  
+private fun getRPCIncognitoGroup(  
+    connectionsPreferences: ConnectionsPreferences,  
+    enabled: Boolean,  
+): Preference.PreferenceGroup {  
+    val getCategories = remember { Injekt.get<GetCategories>() }  
+    val allCategories by getCategories.subscribe().collectAsState(initial = runBlocking { getCategories.await() })  
+
+    val discordRPCIncognitoPref = connectionsPreferences.discordRPCIncognito()  
+    val discordRPCIncognitoCategoriesPref = connectionsPreferences.discordRPCIncognitoCategories()  
+
+    val includedManga by discordRPCIncognitoCategoriesPref.collectAsState()  
+    var showDialog by rememberSaveable { mutableStateOf(false) }  
+    if (showDialog) {  
+        TriStateListDialog(  
+            title = stringResource(MR.strings.general_categories),  
+            message = stringResource(MR.strings.pref_discord_incognito_categories_details),  
+            items = allCategories,  
+            initialChecked = includedManga.mapNotNull { id -> allCategories.find { it.id.toString() == id } },  
+            initialInversed = includedManga.mapNotNull { allCategories.find { false } },  
+            itemLabel = { it.visualName },  
+            onDismissRequest = { showDialog = false },  
+            onValueChanged = { newIncluded, _ ->  
+                discordRPCIncognitoCategoriesPref.set(  
+                    newIncluded.fastMap { it.id.toString() }  
+                        .toSet(),  
+                )  
+                showDialog = false  
+            },  
+            onlyChecked = true,  
+        )  
+    }  
+
+    return Preference.PreferenceGroup(  
+        title = stringResource(MR.strings.general_categories),  
+        preferenceItems = persistentListOf(  
+            Preference.PreferenceItem.SwitchPreference(  
+                preference = discordRPCIncognitoPref,  
+                title = stringResource(MR.strings.pref_discord_incognito),  
+                subtitle = stringResource(MR.strings.pref_discord_incognito_summary),  
+            ),  
+            Preference.PreferenceItem.TextPreference(  
+                title = stringResource(MR.strings.general_categories),  
+                subtitle = getCategoriesLabel(  
+                    allCategories = allCategories,  
+                    included = includedManga,  
+                ),  
+                onClick = { showDialog = true },  
+            ),  
+            Preference.PreferenceItem.InfoPreference(stringResource(MR.strings.pref_discord_incognito_categories_details)),  
+        ),  
+        enabled = enabled,  
+    )  
 }
 
-@Composable
-private fun getRPCIncognitoGroup(
-    connectionsPreferences: ConnectionsPreferences,
-    enabled: Boolean,
-) {
-    val getCategories = remember { Injekt.get<GetCategories>() }
-    val allCategories by getCategories.subscribe().collectAsState(initial = runBlocking { getCategories.await() })
-
-    val discordRPCIncognitoPref = connectionsPreferences.discordRPCIncognito()
-    val discordRPCIncognitoCategoriesPref = connectionsPreferences.discordRPCIncognitoCategories()
-
-    val includedManga by discordRPCIncognitoCategoriesPref.collectAsState()
-    var showDialog by rememberSaveable { mutableStateOf(false) }
-    if (showDialog) {
-        TriStateListDialog(
-            title = stringResource(MR.strings.general_categories),
-            message = stringResource(MR.strings.pref_discord_incognito_categories_details),
-            items = allCategories,
-            initialChecked = includedManga.mapNotNull { id -> allCategories.find { it.id.toString() == id } },
-            initialInversed = includedManga.mapNotNull { allCategories.find { false } },
-            itemLabel = { it.visualName },
-            onDismissRequest = { showDialog = false },
-            onValueChanged = { newIncluded, _ ->
-                discordRPCIncognitoCategoriesPref.set(
-                    newIncluded.fastMap { it.id.toString() }
-                        .toSet(),
-                )
-                showDialog = false
-            },
-            onlyChecked = true,
-        )
-    }
-
-    PreferenceGroup(
-        title = stringResource(MR.strings.general_categories),
-        preferenceItems = persistentListOf(
-            Preference.PreferenceItem.SwitchPreference(
-                preference = discordRPCIncognitoPref,
-                title = stringResource(MR.strings.pref_discord_incognito),
-                subtitle = stringResource(MR.strings.pref_discord_incognito_summary),
-            ),
-            Preference.PreferenceItem.TextPreference(
-                title = stringResource(MR.strings.general_categories),
-                subtitle = getCategoriesLabel(
-                    allCategories = allCategories,
-                    included = includedManga,
-                ),
-                onClick = { showDialog = true },
-            ),
-            Preference.PreferenceItem.InfoPreference(stringResource(MR.strings.pref_discord_incognito_categories_details)),
-        ),
-        enabled = enabled,
-    )
 }
 
-@Composable
-fun ConnectionsLogoutDialog(
-    service: ConnectionsService,
-    onDismissRequest: () -> Unit,
-) {
-    val context = LocalContext.current
-    val router = LocalBackPress.current
-
-    AlertDialog(
-        onDismissRequest = onDismissRequest,
-        title = {
-            Text(
-                text = stringResource(MR.strings.logout_title, stringResource(service.nameRes())),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth(),
-            )
-        },
-        confirmButton = {
-            Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-                OutlinedButton(
-                    modifier = Modifier.weight(1f),
-                    onClick = onDismissRequest,
-                ) {
-                    Text(text = stringResource(MR.strings.action_cancel))
-                }
-                Button(
-                    modifier = Modifier.weight(1f),
-                    onClick = {
-                        service.logout()
-                        onDismissRequest()
-                        context.toast(MR.strings.logout_success)
-                        router?.invoke() // Usar el operador ?.invoke para llamar a la función de manera segura
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.error,
-                        contentColor = MaterialTheme.colorScheme.onError,
-                    ),
-                ) {
-                    Text(text = stringResource(MR.strings.logout))
-                }
-            }
-        },
-    )
-}
