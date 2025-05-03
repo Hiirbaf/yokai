d3a9dc7c7bad9fd81390b352bc7d7df885bdbc0c
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-03 19:42:19 -0300

    Update SettingsDiscordScreen.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
@@ -1,84 +1,72 @@
+// AM -->
 package eu.kanade.tachiyomi.ui.setting.controllers
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.stringResource
-import cafe.adriel.voyager.hilt.getViewModel
-import cafe.adriel.voyager.navigator.LocalNavigator
-import eu.kanade.presentation.scene.settings.SettingsScene
+import android.app.Activity
+import android.content.Intent
+import androidx.preference.PreferenceGroup
+import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.ui.setting.SettingsViewModel
-import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.persistentMapOf
-import tachiyomi.domain.connections.ConnectionsPreferences
-import tachiyomi.domain.connections.service.ConnectionsManager
-import tachiyomi.i18n.MR
-import tachiyomi.presentation.core.components.material.dialog.Dialog
-import tachiyomi.presentation.core.components.material.dialog.LogoutConnectionsDialog
-import yokai.presentation.component.preference.Preference
-import yokai.presentation.component.preference.PreferenceScreen
+import eu.kanade.tachiyomi.data.connections.ConnectionsManager
+import eu.kanade.tachiyomi.data.connections.ConnectionsService
+import eu.kanade.tachiyomi.data.preference.PreferenceKeys
+import eu.kanade.tachiyomi.ui.base.controller.pushController
+import eu.kanade.tachiyomi.ui.setting.connections.DiscordLoginActivity
+import eu.kanade.tachiyomi.util.preference.add
+import eu.kanade.tachiyomi.util.preference.iconRes
+import eu.kanade.tachiyomi.util.preference.onClick
+import eu.kanade.tachiyomi.util.preference.titleRes
+import eu.kanade.tachiyomi.widget.preference.TrackerPreference
+import eu.kanade.tachiyomi.ui.setting.SettingsLegacyController
+import uy.kohesive.injekt.injectLazy
+import eu.kanade.tachiyomi.data.preference.PreferenceKeys as Keys
 
-@Composable
-fun SettingsDiscordScreen() {
-    val navigator = LocalNavigator.current
-    val settingsViewModel = getViewModel<SettingsViewModel>()
-    val connectionsManager = settingsViewModel.connectionsManager
-    val enableDRPCPref = settingsViewModel.enableDiscordRPCPref
-    val useChapterTitlesPref = settingsViewModel.useChapterTitlesPref
-    val discordRPCStatus = settingsViewModel.discordRPCStatusPref
+class SettingsConnectionsController : SettingsLegacyController() {
 
-    val enableDRPC by enableDRPCPref.collectAsState()
+    private val connectionsManager: ConnectionsManager by injectLazy()
 
-    var dialog by remember { mutableStateOf<Dialog?>(null) }
+    override fun setupPreferenceScreen(screen: PreferenceScreen) = screen.apply {
+        titleRes = R.string.pref_category_connections
 
-    SettingsScene(
-        title = stringResource(MR.strings.pref_category_connections),
-        onBackPressed = { navigator?.pop() },
-    ) {
-        PreferenceScreen(
-            items = persistentListOf(
-                Preference.PreferenceGroup(
-                    title = stringResource(MR.strings.connections_discord),
-                    preferenceItems = persistentListOf(
-                        Preference.PreferenceItem.SwitchPreference(
-                            pref = enableDRPCPref,
-                            title = stringResource(MR.strings.pref_enable_discord_rpc),
-                        ),
-                        Preference.PreferenceItem.SwitchPreference(
-                            pref = useChapterTitlesPref,
-                            enabled = enableDRPC,
-                            title = stringResource(MR.strings.show_chapters_titles_title),
-                            subtitle = stringResource(MR.strings.show_chapters_titles_subtitle),
-                        ),
-                        Preference.PreferenceItem.ListPreference(
-                            pref = discordRPCStatus,
-                            title = stringResource(MR.strings.pref_discord_status),
-                            entries = persistentMapOf(
-                                -1 to stringResource(MR.strings.pref_discord_dnd),
-                                0 to stringResource(MR.strings.pref_discord_idle),
-                                1 to stringResource(MR.strings.pref_discord_online),
-                            ),
-                            enabled = enableDRPC,
-                        ),
-                    ),
-                ),
-                Preference.PreferenceGroup(
-                    title = stringResource(MR.strings.pref_category_misc),
-                    preferenceItems = persistentListOf(
-                        Preference.PreferenceItem.TextPreference(
-                            title = stringResource(MR.strings.logout),
-                            onClick = {
-                                dialog = LogoutConnectionsDialog(connectionsManager.discord)
-                            },
-                        ),
-                    ),
-                ),
-            ),
+        connectionPreference(connectionsManager.discord, SettingsDiscordController()) {
+            val intent = Intent(activity!!, DiscordLoginActivity::class.java)
+            startActivityForResult(intent, connectionsManager.discord.id.toInt())
+        }
+    }
+
+    private inline fun PreferenceGroup.connectionPreference(
+        service: ConnectionsService,
+        controller: SettingsController,
+        crossinline login: () -> Unit,
+    ): TrackerPreference {
+        return add(
+            TrackerPreference(context).apply {
+                key = Keys.connectionToken(service.id)
+                titleRes = service.nameRes()
+                iconRes = service.getLogo()
+                iconColor = service.getLogoColor()
+                onClick {
+                    if (service.isLogged) router.pushController(controller)
+                    else login()
+                }
+            },
         )
+    }
 
-        dialog?.Content()
+    override fun onActivityResumed(activity: Activity) {
+        super.onActivityResumed(activity)
+        updatePreference(connectionsManager.discord.id)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                connectionsManager.discord.id.toInt() -> router.pushController(SettingsDiscordController())
+            }
+        }
+    }
+
+    private fun updatePreference(id: Long) {
+        val pref = findPreference(PreferenceKeys.connectionToken(id)) as? TrackerPreference
+        pref?.notifyChanged()
     }
 }
+// AM <--
