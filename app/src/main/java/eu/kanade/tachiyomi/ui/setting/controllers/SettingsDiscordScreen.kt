41abaf85e9c53e59a0678d9a3773d00893f23820
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-03 13:53:59 -0300

    Update SettingsDiscordScreen.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
@@ -32,138 +32,77 @@ import uy.kohesive.injekt.api.get
 
 object SettingsDiscordScreen : BaseComposeController {
 
-@ReadOnlyComposable  
-@Composable  
-override fun getTitleRes() = MR.strings.pref_category_connections  
+@ReadOnlyComposable
+    @Composable
+    override fun getTitleRes() = MR.strings.pref_category_connections
 
-@Composable  
-override fun RowScope.AppBarAction() {  
-    val uriHandler = LocalUriHandler.current  
-    IconButton(onClick = { uriHandler.openUri("https://tachiyomi.org/help/guides/tracking/") }) {  
-        Icon(  
-            imageVector = Icons.AutoMirrored.Outlined.HelpOutline,  
-            contentDescription = stringResource(MR.strings.tracking_guide),  
-        )  
-    }  
-}  
+    @Composable
+    override fun RowScope.AppBarAction() {
+        val uriHandler = LocalUriHandler.current
+        IconButton(onClick = { uriHandler.openUri("https://tachiyomi.org/help/guides/tracking/") }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.HelpOutline,
+                contentDescription = stringResource(MR.strings.tracking_guide),
+            )
+        }
+    }
 
-@Composable  
-override fun getPreferences(): List<Preference> {  
-    val connectionsPreferences = remember { Injekt.get<ConnectionsPreferences>() }  
-    val connectionsManager = remember { Injekt.get<ConnectionsManager>() }  
-    val enableDRPCPref = connectionsPreferences.enableDiscordRPC()  
-    val useChapterTitlesPref = connectionsPreferences.useChapterTitles()  
-    val discordRPCStatus = connectionsPreferences.discordRPCStatus()  
+    @Composable
+    override fun getPreferences(): List<Preference> {
+        val connectionsPreferences = remember { Injekt.get<ConnectionsPreferences>() }
+        val connectionsManager = remember { Injekt.get<ConnectionsManager>() }
+        val enableDRPCPref = connectionsPreferences.enableDiscordRPC()
+        val useChapterTitlesPref = connectionsPreferences.useChapterTitles()
+        val discordRPCStatus = connectionsPreferences.discordRPCStatus()
 
-    val enableDRPC by enableDRPCPref.collectAsState()  
-    val useChapterTitles by useChapterTitlesPref.collectAsState()  
+        val enableDRPC by enableDRPCPref.collectAsState()
+        val useChapterTitles by useChapterTitlesPref.collectAsState()
 
-    var dialog by remember { mutableStateOf<Any?>(null) }  
-    dialog?.run {  
-        when (this) {  
-            is LogoutConnectionsDialog -> {  
-                ConnectionsLogoutDialog(  
-                    service = service,  
-                    onDismissRequest = {  
-                        dialog = null  
-                        enableDRPCPref.set(false)  
-                    },  
-                )  
-            }  
-        }  
-    }  
+        var dialog by remember { mutableStateOf<Any?>(null) }
+        dialog?.run {
+            when (this) {
+                is LogoutConnectionsDialog -> {
+                    ConnectionsLogoutDialog(
+                        service = service,
+                        onDismissRequest = {
+                            dialog = null
+                            enableDRPCPref.set(false)
+                        },
+                    )
+                }
+            }
+        }
 
-    return listOf(  
-        Preference.PreferenceGroup(  
-            title = stringResource(MR.strings.connections_discord),  
-            preferenceItems = persistentListOf(  
-                Preference.PreferenceItem.SwitchPreference(  
-                    preference = enableDRPCPref,  
-                    title = stringResource(MR.strings.pref_enable_discord_rpc),  
-                ),  
-                Preference.PreferenceItem.SwitchPreference(  
-                    preference = useChapterTitlesPref,  
-                    enabled = enableDRPC,  
-                    title = stringResource(MR.strings.show_chapters_titles_title),  
-                    subtitle = stringResource(MR.strings.show_chapters_titles_subtitle),  
-                ),  
-                Preference.PreferenceItem.ListPreference(  
-                    preference = discordRPCStatus,  
-                    title = stringResource(MR.strings.pref_discord_status),  
-                    entries = persistentMapOf(  
-                        -1 to stringResource(MR.strings.pref_discord_dnd),  
-                        0 to stringResource(MR.strings.pref_discord_idle),  
-                        1 to stringResource(MR.strings.pref_discord_online),  
-                    ),  
-                    enabled = enableDRPC,  
-                ),  
-            ),  
-        ),  
-        getRPCIncognitoGroup(  
-            connectionsPreferences = connectionsPreferences,  
-            enabled = enableDRPC,  
-        ),  
-        Preference.PreferenceItem.TextPreference(  
-            title = stringResource(MR.strings.logout),  
-            onClick = { dialog = LogoutConnectionsDialog(connectionsManager.discord) },  
-        ),  
-    )  
-}  
-
-@Composable  
-private fun getRPCIncognitoGroup(  
-    connectionsPreferences: ConnectionsPreferences,  
-    enabled: Boolean,  
-): Preference.PreferenceGroup {  
-    val getCategories = remember { Injekt.get<GetCategories>() }  
-    val allCategories by getCategories.subscribe().collectAsState(initial = runBlocking { getCategories.await() })  
-
-    val discordRPCIncognitoPref = connectionsPreferences.discordRPCIncognito()  
-    val discordRPCIncognitoCategoriesPref = connectionsPreferences.discordRPCIncognitoCategories()  
-
-    val includedManga by discordRPCIncognitoCategoriesPref.collectAsState()  
-    var showDialog by rememberSaveable { mutableStateOf(false) }  
-    if (showDialog) {  
-        TriStateListDialog(  
-            title = stringResource(MR.strings.general_categories),  
-            message = stringResource(MR.strings.pref_discord_incognito_categories_details),  
-            items = allCategories,  
-            initialChecked = includedManga.mapNotNull { id -> allCategories.find { it.id.toString() == id } },  
-            initialInversed = includedManga.mapNotNull { allCategories.find { false } },  
-            itemLabel = { it.visualName },  
-            onDismissRequest = { showDialog = false },  
-            onValueChanged = { newIncluded, _ ->  
-                discordRPCIncognitoCategoriesPref.set(  
-                    newIncluded.fastMap { it.id.toString() }  
-                        .toSet(),  
-                )  
-                showDialog = false  
-            },  
-            onlyChecked = true,  
-        )  
-    }  
-
-    return Preference.PreferenceGroup(  
-        title = stringResource(MR.strings.general_categories),  
-        preferenceItems = persistentListOf(  
-            Preference.PreferenceItem.SwitchPreference(  
-                preference = discordRPCIncognitoPref,  
-                title = stringResource(MR.strings.pref_discord_incognito),  
-                subtitle = stringResource(MR.strings.pref_discord_incognito_summary),  
-            ),  
-            Preference.PreferenceItem.TextPreference(  
-                title = stringResource(MR.strings.general_categories),  
-                subtitle = getCategoriesLabel(  
-                    allCategories = allCategories,  
-                    included = includedManga,  
-                ),  
-                onClick = { showDialog = true },  
-            ),  
-            Preference.PreferenceItem.InfoPreference(stringResource(MR.strings.pref_discord_incognito_categories_details)),  
-        ),  
-        enabled = enabled,  
-    )  
+        return listOf(
+            Preference.PreferenceGroup(
+                title = stringResource(MR.strings.connections_discord),
+                preferenceItems = persistentListOf(
+                    Preference.PreferenceItem.SwitchPreference(
+                        preference = enableDRPCPref,
+                        title = stringResource(MR.strings.pref_enable_discord_rpc),
+                    ),
+                    Preference.PreferenceItem.SwitchPreference(
+                        preference = useChapterTitlesPref,
+                        enabled = enableDRPC,
+                        title = stringResource(MR.strings.show_chapters_titles_title),
+                        subtitle = stringResource(MR.strings.show_chapters_titles_subtitle),
+                    ),
+                    Preference.PreferenceItem.ListPreference(
+                        preference = discordRPCStatus,
+                        title = stringResource(MR.strings.pref_discord_status),
+                        entries = persistentMapOf(
+                            -1 to stringResource(MR.strings.pref_discord_dnd),
+                            0 to stringResource(MR.strings.pref_discord_idle),
+                            1 to stringResource(MR.strings.pref_discord_online),
+                        ),
+                        enabled = enableDRPC,
+                    ),
+                ),
+            ),
+            Preference.PreferenceItem.TextPreference(
+                title = stringResource(MR.strings.logout),
+                onClick = { dialog = LogoutConnectionsDialog(connectionsManager.discord) },
+            ),
+        )
+    }
 }
-
-}
-
