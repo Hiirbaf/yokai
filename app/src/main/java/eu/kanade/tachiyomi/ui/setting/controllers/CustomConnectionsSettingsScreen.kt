86eb0539a29fb0164e6603ac48f937e2ba586569
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-03 05:05:24 -0300

    Update CustomConnectionsSettingsScreen.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/CustomConnectionsSettingsScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/CustomConnectionsSettingsScreen.kt
@@ -1,43 +1,36 @@
-// CustomConnectionsSettingsScreen.kt
 package eu.kanade.tachiyomi.setting.controllers
 
+import android.content.Context
 import androidx.compose.runtime.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.*
-import androidx.compose.ui.text.input.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.*
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import dev.icerock.moko.resources.StringResource
+import dev.icerock.moko.resources.compose.stringResource
 import eu.kanade.tachiyomi.data.connections.ConnectionsManager
 import eu.kanade.tachiyomi.data.connections.ConnectionsService
-import eu.kanade.tachiyomi.util.system.openDiscordLoginActivity
 import eu.kanade.tachiyomi.util.system.launchIO
-import yokai.i18n.MR
-import dev.icerock.moko.resources.compose.stringResource
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
-import androidx.compose.ui.res.stringResource as stringResourceInt
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.material.icons.outlined.Close
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import android.content.Context
-import androidx.compose.ui.text.style.TextAlign
+import eu.kanade.tachiyomi.util.system.openDiscordLoginActivity
 import eu.kanade.tachiyomi.util.system.toast
 import eu.kanade.tachiyomi.util.system.withUIContext
-import dev.icerock.moko.resources.StringResource
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+import yokai.i18n.MR
 
 object CustomConnectionsSettingsScreen : Screen {
 
@@ -88,12 +81,12 @@ object CustomConnectionsSettingsScreen : Screen {
                         modifier = Modifier.weight(1f),
                     )
                     IconButton(onClick = {
-    dialog = LoginConnectionsDialog(
-        service = connectionsManager.discord,
-        uNameStringRes = MR.strings.username,
-    )
-}) {
-    Icon(Icons.Default.Settings, contentDescription = null)
+                        dialog = LoginConnectionsDialog(
+                            service = connectionsManager.discord,
+                            uNameStringRes = MR.strings.username,
+                        )
+                    }) {
+                        Icon(Icons.Default.Settings, contentDescription = null)
                     }
                 }
             }
@@ -101,129 +94,112 @@ object CustomConnectionsSettingsScreen : Screen {
     }
 }
 
-    @Composable
-    private fun ConnectionsLoginDialog(
-        service: ConnectionsService,
-        @StringRes uNameStringRes: Int,
-        onDismissRequest: () -> Unit,
-    ) {
-        val context = LocalContext.current
-        val scope = rememberCoroutineScope()
+@Composable
+private fun ConnectionsLoginDialog(
+    service: ConnectionsService,
+    uNameStringRes: StringResource,
+    onDismissRequest: () -> Unit,
+) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
 
-        var username by remember { mutableStateOf(TextFieldValue(service.getUsername())) }
-        var password by remember { mutableStateOf(TextFieldValue(service.getPassword())) }
-        var processing by remember { mutableStateOf(false) }
-        var inputError by remember { mutableStateOf(false) }
+    var username by remember { mutableStateOf(TextFieldValue(service.getUsername())) }
+    var password by remember { mutableStateOf(TextFieldValue(service.getPassword())) }
+    var processing by remember { mutableStateOf(false) }
+    var inputError by remember { mutableStateOf(false) }
 
-        AlertDialog(
-            onDismissRequest = onDismissRequest,
-            title = {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text(
-                        text = stringResource(
-                            MR.strings.login_title,
-                            stringResource(service.nameRes()),
-                        ),
-                        modifier = Modifier.weight(1f),
-                    )
-                    IconButton(onClick = onDismissRequest) {
-                        Icon(
-                            imageVector = Icons.Outlined.Close,
-                            contentDescription = stringResource(MR.strings.action_close),
-                        )
-                    }
-                }
-            },
-            text = {
-                Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = username,
-                        onValueChange = { username = it },
-                        label = { Text(text = stringResourceInt(uNameStringRes)) },
-                        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-                        singleLine = true,
-                        isError = inputError && username.text.isEmpty(),
-                    )
-
-                    var hidePassword by remember { mutableStateOf(true) }
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = password,
-                        onValueChange = { password = it },
-                        label = { Text(text = stringResource(MR.strings.password)) },
-                        trailingIcon = {
-                            IconButton(onClick = { hidePassword = !hidePassword }) {
-                                Icon(
-                                    imageVector = if (hidePassword) {
-                                        Icons.Filled.Visibility
-                                    } else {
-                                        Icons.Filled.VisibilityOff
-                                    },
-                                    contentDescription = null,
-                                )
-                            }
-                        },
-                        visualTransformation = if (hidePassword) {
-                            PasswordVisualTransformation()
-                        } else {
-                            VisualTransformation.None
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Password,
-                            imeAction = ImeAction.Done,
-                        ),
-                        singleLine = true,
-                        isError = inputError && password.text.isEmpty(),
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = stringResource(
+                        MR.strings.login_title,
+                        stringResource(service.nameRes()),
+                    ),
+                    modifier = Modifier.weight(1f),
+                )
+                IconButton(onClick = onDismissRequest) {
+                    Icon(
+                        imageVector = Icons.Outlined.Close,
+                        contentDescription = stringResource(MR.strings.action_close),
                     )
                 }
-            },
-            confirmButton = {
-                Button(
+            }
+        },
+        text = {
+            Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+                OutlinedTextField(
                     modifier = Modifier.fillMaxWidth(),
-                    enabled = !processing,
-                    onClick = {
-                        if (username.text.isEmpty() || password.text.isEmpty()) {
-                            inputError = true
-                            return@Button
-                        }
-                        scope.launchIO {
-                            inputError = false
-                            processing = true
-                            val result = checkLogin(
-                                context = context,
-                                service = service,
-                                username = username.text,
-                                password = password.text,
+                    value = username,
+                    onValueChange = { username = it },
+                    label = { Text(text = stringResource(uNameStringRes)) },
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                    singleLine = true,
+                    isError = inputError && username.text.isEmpty(),
+                )
+
+                var hidePassword by remember { mutableStateOf(true) }
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text(text = stringResource(MR.strings.password)) },
+                    trailingIcon = {
+                        IconButton(onClick = { hidePassword = !hidePassword }) {
+                            Icon(
+                                imageVector = if (hidePassword) {
+                                    Icons.Filled.Visibility
+                                } else {
+                                    Icons.Filled.VisibilityOff
+                                },
+                                contentDescription = null,
                             )
-                            if (result) onDismissRequest()
-                            processing = false
                         }
                     },
-                ) {
-                    val id = if (processing) MR.strings.loading else MR.strings.login
-                    Text(text = stringResource(id))
-                }
-            },
-        )
-    }
-
-private suspend fun checkLogin(
-        context: Context,
-        service: ConnectionsService,
-        username: String,
-        password: String,
-    ): Boolean {
-        return try {
-            service.login(username, password)
-            withUIContext { context.toast(MR.strings.login_success) }
-            true
-        } catch (e: Throwable) {
-            service.logout()
-            withUIContext { context.toast(e.message.toString()) }
-            false
-        }
-    }
+                    visualTransformation = if (hidePassword) {
+                        PasswordVisualTransformation()
+                    } else {
+                        VisualTransformation.None
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done,
+                    ),
+                    singleLine = true,
+                    isError = inputError && password.text.isEmpty(),
+                )
+            }
+        },
+        confirmButton = {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                enabled = !processing,
+                onClick = {
+                    if (username.text.isEmpty() || password.text.isEmpty()) {
+                        inputError = true
+                        return@Button
+                    }
+                    scope.launchIO {
+                        inputError = false
+                        processing = true
+                        val result = checkLogin(
+                            context = context,
+                            service = service,
+                            username = username.text,
+                            password = password.text,
+                        )
+                        if (result) onDismissRequest()
+                        processing = false
+                    }
+                },
+            ) {
+                val id = if (processing) MR.strings.loading else MR.strings.login
+                Text(text = stringResource(id))
+            }
+        },
+    )
+}
 
 @Composable
 internal fun ConnectionsLogoutDialog(
@@ -269,9 +245,26 @@ internal fun ConnectionsLogoutDialog(
     )
 }
 
+private suspend fun checkLogin(
+    context: Context,
+    service: ConnectionsService,
+    username: String,
+    password: String,
+): Boolean {
+    return try {
+        service.login(username, password)
+        withUIContext { context.toast(MR.strings.login_success) }
+        true
+    } catch (e: Throwable) {
+        service.logout()
+        withUIContext { context.toast(e.message.toString()) }
+        false
+    }
+}
+
 private data class LoginConnectionsDialog(
     val service: ConnectionsService,
-    @StringRes val uNameStringRes: Int,
+    val uNameStringRes: StringResource,
 )
 
 internal data class LogoutConnectionsDialog(
