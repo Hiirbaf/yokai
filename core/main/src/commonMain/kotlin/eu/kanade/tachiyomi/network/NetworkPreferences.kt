c91725088fa8f9d0a4e7dd904e7436fee8065609
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-07 12:20:59 -0300

    Update NetworkPreferences.kt

--- core/main/src/commonMain/kotlin/eu/kanade/tachiyomi/network/NetworkPreferences.kt
+++ core/main/src/commonMain/kotlin/eu/kanade/tachiyomi/network/NetworkPreferences.kt
@@ -1,17 +1,27 @@
 package eu.kanade.tachiyomi.network
 
+import android.content.Context
+import android.webkit.WebSettings
 import eu.kanade.tachiyomi.core.preference.PreferenceStore
 
 class NetworkPreferences(
     private val preferenceStore: PreferenceStore,
     private val verboseLogging: Boolean,
+    private val context: Context, // Context necesario para obtener el User-Agent del sistema
 ) {
 
     fun verboseLogging() = preferenceStore.getBoolean("verbose_logging", verboseLogging)
 
     fun dohProvider() = preferenceStore.getInt("doh_provider", -1)
 
-    fun defaultUserAgent() = preferenceStore.getString("default_user_agent", DEFAULT_USER_AGENT)
+    fun defaultUserAgent(): String {
+        val fallback = try {
+            WebSettings.getDefaultUserAgent(context)
+        } catch (e: Exception) {
+            DEFAULT_USER_AGENT
+        }
+        return preferenceStore.getString("default_user_agent", fallback)
+    }
 
     companion object {
         const val DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0"
