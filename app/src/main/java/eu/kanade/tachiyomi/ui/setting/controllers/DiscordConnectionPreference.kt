9ab0e1b11d279da4b3fe8e13e65a37029da923c5
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-07-21 20:52:28 -0300

    Create DiscordConnectionPreference.kt

---
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/DiscordConnectionPreference.kt
@@ -0,0 +1,37 @@
+package eu.kanade.tachiyomi.ui.setting.controllers
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.preference.Preference
+import androidx.preference.PreferenceViewHolder
+import tachiyomi.R
+
+class DiscordConnectionPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : Preference(context, attrs) {
+
+    var onLoginClick: (() -> Unit)? = null
+    var onSettingsClick: (() -> Unit)? = null
+
+    init {
+        layoutResource = R.layout.preference_discord_connection
+    }
+
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
+        super.onBindViewHolder(holder)
+
+        val titleView = holder.findViewById(R.id.discord_title) as? TextView
+        val loginButton = holder.findViewById(R.id.discord_login) as? Button
+        val settingsButton = holder.findViewById(R.id.discord_settings) as? Button
+
+        titleView?.text = title
+
+        loginButton?.setOnClickListener { onLoginClick?.invoke() }
+        settingsButton?.setOnClickListener { onSettingsClick?.invoke() }
+    }
+}
