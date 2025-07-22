f5b895e84d8fa046c79413c4ea2e96092971b95b
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-07-22 17:03:12 -0300

    Update DiscordRPCService.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/connection/discord/DiscordRPCService.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/connection/discord/DiscordRPCService.kt
@@ -10,7 +10,7 @@ import android.os.Looper
 import androidx.compose.ui.util.fastAny
 import eu.kanade.domain.connections.service.ConnectionsPreferences
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.data.connections.ConnectionsManager
+import eu.kanade.tachiyomi.data.connections.ConnectionManager
 import eu.kanade.tachiyomi.data.notification.NotificationReceiver
 import eu.kanade.tachiyomi.data.notification.Notifications
 import eu.kanade.tachiyomi.network.GET
