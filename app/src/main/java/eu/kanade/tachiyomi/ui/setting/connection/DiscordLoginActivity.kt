c889c892b6f9d0d29d23f2e798df82815d48705f
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-07-21 22:56:53 -0300

    Update DiscordLoginActivity.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/connection/DiscordLoginActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/connection/DiscordLoginActivity.kt
@@ -1,86 +1,64 @@
-// AM (DISCORD_RPC) -->
 package eu.kanade.tachiyomi.ui.setting.connection
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.ViewGroup
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
-import eu.kanade.domain.connection.service.ConnectionPreferences
-import eu.kanade.tachiyomi.data.connection.ConnectionManager
+import yokai.domain.connection.service.ConnectionsPreferences
+import yokai.R
+import eu.kanade.tachiyomi.data.connections.ConnectionsManager
 import eu.kanade.tachiyomi.ui.base.activity.BaseActivity
 import eu.kanade.tachiyomi.util.system.toast
-import eu.kanade.tachiyomi.util.view.setComposeContent
 import yokai.i18n.MR
 import uy.kohesive.injekt.injectLazy
 import java.io.File
 
 class DiscordLoginActivity : BaseActivity() {
 
-    private val connectionManager: ConnectionManager by injectLazy()
-    private val connectionPreferences: ConnectionPreferences by injectLazy()
+    private val connectionsManager: ConnectionsManager by injectLazy()
+    private val connectionsPreferences: ConnectionsPreferences by injectLazy()
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setComposeContent { DiscordWebview(this::login) }
+        setContentView(R.layout.discord_login_activity)
+        val webView = findViewById<WebView>(R.id.webview)
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.databaseEnabled = true
+            settings.domStorageEnabled = true
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                if (url != null && url.endsWith("/app")) {
+                    webView.stopLoading()
+                    webView.evaluateJavascript(
+                        """
+                        (function() {
+                            const wreq = (webpackChunkdiscord_app.push([[''], {}, e => { m = []; for (let c in e.c) m.push(e.c[c])}]), m)
+                            webpackChunkdiscord_app.pop()
+                            const token = wreq.find(m => m?.exports?.default?.getToken !== void 0).exports.default.getToken(); 
+                            return token;
+                        })()
+                        """.trimIndent(),
+                    ) {
+                        login(it.trim('"'))
+                    }
+                }
+            }
+        }
+        webView.loadUrl("https://discord.com/login")
     }
 
     private fun login(token: String) {
-        connectionPreferences.connectionToken(connectionManager.discord).set(token)
-        connectionPreferences.setConnectionCredentials(connectionManager.discord, "Discord", "Logged In")
+        connectionsPreferences.connectionsToken(connectionsManager.discord).set(token)
+        connectionsPreferences.setConnectionsCredentials(connectionsManager.discord, "Discord", "Logged In")
         toast(MR.strings.login_success)
+        Log.d("discord_login_tachiyomisy", "Logged in with token: $token")
         applicationInfo.dataDir.let { File("$it/app_webview/").deleteRecursively() }
         finish()
     }
 }
-
-@SuppressLint("SetJavaScriptEnabled")
-@Composable
-private fun DiscordWebview(
-    login: (String) -> Unit,
-) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        AndroidView(
-            factory = { context ->
-                WebView(context).apply {
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                    )
-
-                    settings.javaScriptEnabled = true
-                    settings.databaseEnabled = true
-                    settings.domStorageEnabled = true
-
-                    webViewClient = object : WebViewClient() {
-                        override fun onPageFinished(view: WebView?, url: String?) {
-                            if (url != null && url.endsWith("/app")) {
-                                stopLoading()
-                                evaluateJavascript(
-                                    """
-                        (function() {
-                            const wreq = (webpackChunkdiscord_app.push([[''], {}, e => { m = []; for (let c in e.c) m.push(e.c[c])}]), m)
-                            webpackChunkdiscord_app.pop()
-                            const token = wreq.find(m => m?.exports?.default?.getToken !== void 0).exports.default.getToken();
-                            return token;
-                        })()
-                                    """.trimIndent(),
-                                ) {
-                                    login(it.trim('"'))
-                                }
-                            }
-                        }
-                    }
-                    loadUrl("https://discord.com/login")
-                }
-            },
-        )
-    }
-}
-// <-- AM (DISCORD_RPC)
