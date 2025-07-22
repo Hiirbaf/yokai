cfec0ccc622560a9403ee30f3fecb7494d261246
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-07-21 21:44:56 -0300

    Create ConnectionManager.kt

---
+++ app/src/main/java/eu/kanade/tachiyomi/data/connection/ConnectionManager.kt
@@ -0,0 +1,16 @@
+package eu.kanade.tachiyomi.data.connection
+
+import eu.kanade.tachiyomi.data.connection.discord.Discord
+
+class ConnectionsManager {
+
+    companion object {
+        const val DISCORD = 201L
+    }
+
+    val discord = Discord(DISCORD)
+
+    val services = listOf(discord)
+
+    fun getService(id: Long) = services.find { it.id == id }
+}
