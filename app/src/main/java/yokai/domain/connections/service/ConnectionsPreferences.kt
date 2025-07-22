2d65e18138ab28db7eb1e99994f9561e10711ee3
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-07-22 00:42:56 -0300

    Update ConnectionsPreferences.kt

--- app/src/main/java/yokai/domain/connections/service/ConnectionsPreferences.kt
+++ app/src/main/java/yokai/domain/connections/service/ConnectionsPreferences.kt
@@ -1,7 +1,7 @@
 package yokai.domain.connections.service
 
 import eu.kanade.tachiyomi.data.connection.ConnectionsService
-import tachiyomi.core.common.preference.PreferenceStore
+import eu.kanade.tachiyomi.core.preference.PreferenceStore
 
 class ConnectionsPreferences(
     private val preferenceStore: PreferenceStore,
