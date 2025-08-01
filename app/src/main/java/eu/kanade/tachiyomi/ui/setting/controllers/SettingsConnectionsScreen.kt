da1ee899a403272ad90bebea6edf1ebaf3d0d897
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-01 02:30:38 -0300

    Update SettingsConnectionsScreen.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsConnectionsScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsConnectionsScreen.kt
@@ -39,9 +39,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import eu.kanade.presentation.more.settings.Preference
-import eu.kanade.tachiyomi.data.connections.ConnectionsManager
-import eu.kanade.tachiyomi.data.connections.ConnectionsService
+import yokai.presentation.component.preference.Preference
+import eu.kanade.tachiyomi.data.connection.ConnectionsManager
+import eu.kanade.tachiyomi.data.connection.ConnectionsService
 import eu.kanade.tachiyomi.util.system.openDiscordLoginActivity
 import eu.kanade.tachiyomi.util.system.toast
 import kotlinx.collections.immutable.persistentListOf
