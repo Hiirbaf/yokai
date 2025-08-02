b4e48a38ae2b73a0f4f6d036ac9c1f4df2575758
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-02 03:10:48 -0300

    Update ConnectionLogoIcon.kt

--- app/src/main/java/yokai/presentation/component/ConnectionLogoIcon.kt
+++ app/src/main/java/yokai/presentation/component/ConnectionLogoIcon.kt
@@ -2,11 +2,13 @@ package yokai.presentation.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
