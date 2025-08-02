a40dd867f901b0bfa228f01f662d9b5e59501b08
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-02 02:42:04 -0300

    Update ModifierExtensions.kt

--- app/src/main/java/yokai/util/ModifierExtensions.kt
+++ app/src/main/java/yokai/util/ModifierExtensions.kt
@@ -2,19 +2,24 @@ package yokai.util
 
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.alpha
 import yokai.presentation.theme.SecondaryItemAlpha
 
 fun Modifier.secondaryItemAlpha(): Modifier = this.alpha(SecondaryItemAlpha)
 
 fun Modifier.clickableNoIndication(
-    interactionSource: MutableInteractionSource,
     onLongClick: (() -> Unit)? = null,
     onClick: () -> Unit,
-) = this.combinedClickable(
-    interactionSource = interactionSource,
-    indication = null,
-    onLongClick = onLongClick,
-    onClick = onClick,
-)
+): Modifier = composed {
+    val interactionSource = remember { MutableInteractionSource() }
+    this.combinedClickable(
+        interactionSource = interactionSource,
+        indication = null,
+        onLongClick = onLongClick,
+        onClick = onClick,
+    )
+}
