ce734379a55ea8143cc12cde73bceebc38225216
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-03 04:11:17 -0300

    Create SettingsDiscordController.kt

---
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordController.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.ui.setting.controllers
+
+import eu.kanade.tachiyomi.ui.setting.SettingsComposeController
+import yokai.presentation.settings.ComposableSettings
+import eu.kanade.tachiyomi.ui.setting.controllers.SettingsDiscordScreen
+
+class SettingsDiscordController : SettingsComposeController() {
+    override fun getComposableSettings(): ComposableSettings = SettingsDiscordScreen
+}
