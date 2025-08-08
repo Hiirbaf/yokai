55444d4d04512c924a8ea551143cc05d980030e3
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-07 21:24:26 -0300

    Update SettingsConnectionsController.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsConnectionsController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsConnectionsController.kt
@@ -5,5 +5,7 @@ import yokai.presentation.settings.ComposableSettings
 import eu.kanade.tachiyomi.ui.setting.controllers.SettingsConnectionsScreen
 
 class SettingsConnectionsController : SettingsComposeController() {
-    override fun getComposableSettings(): ComposableSettings = SettingsConnectionsScreen
+    override fun getComposableSettings(): ComposableSettings {
+        return SettingsConnectionsScreen(router)
+    }
 }
