66ed5737b5c3d3dc3f35475cfafc8139ead469db
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-12 01:08:37 -0300

    Update SettingsDiscordScreen.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/controllers/SettingsDiscordScreen.kt
@@ -116,10 +116,10 @@ object SettingsDiscordScreen : ComposableSettings {
                     ),
                 ),
             ),
-            /*getRPCIncognitoGroup(
+            getRPCIncognitoGroup(
                 connectionsPreferences = connectionsPreferences,
                 enabled = enableDRPC,
-            ),*/
+            ),
             Preference.PreferenceItem.TextPreference(
                 title = stringResource(MR.strings.logout),
                 onClick = { dialog = LogoutConnectionsDialog(connectionsManager.discord) },
