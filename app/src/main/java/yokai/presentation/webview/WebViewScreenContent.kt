8725a2f7a9c42c9352abe8c3950b83e7dfba84b3
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-10-18 13:38:42 -0300

    Add TopAppBar import to WebViewScreenContent.kt

--- app/src/main/java/yokai/presentation/webview/WebViewScreenContent.kt
+++ app/src/main/java/yokai/presentation/webview/WebViewScreenContent.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
